a7cec0472f663efa5cf4a5ffc0436da835a4e610
jonthomas-devpipeline <102564782+jonthomas-devpipeline@users.noreply.github.com>
2022-10-05 20:43:55 +0000

    Update main.py

--- main.py
+++ main.py
@@ -29,12 +29,40 @@ def add_team():
     
 @app.route('/team/<team_name>')
 def get_team_by_name(team_name):
-  results = cursor.execute("SELECT id, name, mascot, city, state, championsWon FROM nflteams WHERE name LIKE %s", [f'%{team_name}%'])
-  results = cursor.fetchall()
-  print(team_name)
+  results = cursor.execute("SELECT id, name, mascot, city, state, championsWon FROM nflteams WHERE LOWER(name) LIKE %s", [f'%{team_name.lower()}%'])
+  results = cursor.fetchone()
                         
-  return jsonify('team'), 200
-                        
+  result_dictionary = {
+    'id' : results[0],
+    'name' : results[1],
+    'mascot' : results[2],
+    'city' : results[3],
+    'state' : results[4],
+    'championshipsWon' : results[5],
+    
+  return jsonify(results), 200
+
+@app.route('/teams', methods=['GET'])
+def get_all_teams():
+    cursor.execute("SELECT id, name, mascot, ciyt, state, championshipsWon FROM nflteams")
+    results = cursor.fetchall()
+    list_of_teams = []
+    form team in results:
+      list_of_teams.append( {
+        'id' : team[0],
+        'name' : team[1],
+        'mascot' : team[2],
+        'city' : team[3],
+        'state' : team[4],
+        'championshipsWon' : team[5],
+      } )
+    
+    output_dictionary = {
+      "teams" : list_of_teams
+    }
+    
+    return jsonify(list_of_teams), 200
+    
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000)
                         
